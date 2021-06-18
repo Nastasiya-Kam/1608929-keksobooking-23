1ce0db619b89826fcd3b7579d4125e91c083a64d
Анастасия <kamanina963@gmail.com>
2021-06-18 07:33:14 +0300

    Реализованы функции для активации и дезактивации формы. Не учитывает инициализацию карты

--- js/form.js
+++ js/form.js
@@ -5,3 +5,26 @@
 //   Если отправка данных прошла успешно, показывается соответствующее сообщение.
 //   Если при отправке данных произошла ошибка запроса, показывается соответствующее сообщение
 // }
+
+function setDisabled (className, isDisabled) {
+  const element = document.querySelector(`.${className}`);
+  const elements = element.children;
+
+  (isDisabled) ? element.classList.add(`${className}--disabled`) : element.classList.remove(`${className}--disabled`);
+
+  for (let index = 0; index < elements.length; index++) {
+    (isDisabled) ? elements[index].disabled = true : elements[index].disabled = false;
+  }
+}
+
+function disablePage () {
+  setDisabled('ad-form', true);
+  setDisabled('map__filters', true);
+}
+
+function enablePage () {
+  setDisabled('ad-form', false);
+  setDisabled('map__filters', false);
+}
+
+export {disablePage, enablePage};
