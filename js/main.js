b6793b36d5b45e9b1442fa6c9f97be28995cab7b
Анастасия <kamanina963@gmail.com>
2021-07-13 20:35:56 +0300

    Доработана подача объявления: в неё можно загружать аватарку и одну фотографию жилья

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {openMessageSuccess} from './messages.js';
 import {setOfferFormSubmit} from './form.js';
+import './preview.js';
 
 setOfferFormSubmit(openMessageSuccess);
--- js/preview.js
+++ js/preview.js
@@ -1,2 +1,49 @@
-// 6.1. В форме подачи объявления показывается превью аватарки пользователя
-// и фотографии помещения при изменении значений соответствующих полей.
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const IMAGE_SIZE = 70;
+
+const chooserAvatar = document.querySelector('.ad-form-header input[type=file]');
+const imgAvatar = document.querySelector('.ad-form-header__preview img');
+const chooserImageProperty = document.querySelector('.ad-form__element input[type=file]');
+const imgProperty = document.querySelector('.ad-form__photo');
+
+const checkFormatFile = (file) => {
+  const fileName = file.name.toLowerCase();
+
+  return FILE_TYPES.some((type) => fileName.endsWith(type));
+};
+
+chooserAvatar.addEventListener('change', () => {
+  const file = chooserAvatar.files[0];
+  const isMatched = checkFormatFile(file);
+
+  if (isMatched) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      imgAvatar.src = reader.result;
+    });
+
+    reader.readAsDataURL(file);
+  }
+});
+
+chooserImageProperty.addEventListener('change', () => {
+  const imageElement = imgAvatar.cloneNode(true);
+  imageElement.width = IMAGE_SIZE;
+  imageElement.height = IMAGE_SIZE;
+
+  const file = chooserImageProperty.files[0];
+  const isMatched = checkFormatFile(file);
+
+  if (isMatched) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      imageElement.src = reader.result;
+      imgProperty.appendChild(imageElement);
+    });
+
+    reader.readAsDataURL(file);
+  }
+
+});
